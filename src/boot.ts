438f2fee87906f1fb74956a34347b535c9895b15
vijesh <vijeshkk@gmail.com>
2017-04-14 22:04:03 +0800

    4th commit

---
+++ src/boot.ts
@@ -0,0 +1,12 @@
+// /// <reference path ="_all.ts" /> 
+
+// module ContactManagerApp {
+//     angular.module('ContactManagerApp', ['ngMaterial','ngMdIcons'] )
+//         .service('userService',UserService)
+//         .controller('mainController',MainController)
+//         .config(($mdThemingProvider: angular.material.IThemingProvider) => {
+//             $mdThemingProvider.theme('default')
+//                 .primaryPalette('blue')
+//                 .accentPalette
+//         });
+// }
